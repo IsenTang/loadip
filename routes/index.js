936ce7294da8cba1029d88051fe8cb474a11565a
isenTang <3104255643@qq.com>
2018-10-01 05:12:44 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', async (ctx, next) => {
   const address = await checkDocker();
 
   await ctx.render('index', {
-    title: `${address}   test cicd`
+    title: `${address}   test cicd11111111111`
   })
 })
 
