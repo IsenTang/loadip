d449cb807ca19cc0f7c9e306d118d3d6b948d7a9
isenTang <3104255643@qq.com>
2018-10-08 05:48:25 +0800

    max

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.get('/', async (ctx, next) => {
   const address = await checkDocker();
 
   await ctx.render('index', {
-    title: `${address}   test cicd test deployment blue/green`
+    title: `${address}   test cicd test deployment blue/green & max`
   })
 })
 
