4ffcc5ce6f020d48e40f486c07d7b2e6184617ee
isenTang <3104255643@qq.com>
2019-01-30 18:13:15 +0800

    t

---
+++ schema/cache.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+
+var cacheSchema = new Schema({
+    context:{type:Object,default:{}}
+});
+
+
+
+
+module.exports = cacheSchema;
